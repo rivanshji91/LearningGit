2ddd44f372a7084c65b287158ba41cb49f2cd0a3
Rivansh <rivanshji91@gmail.com>
2026-03-19 00:18:34 +0530

    new feature added

--- untitled/src/main/java/org/example/ReverseWord.java
+++ untitled/src/main/java/org/example/ReverseWord.java
@@ -1,4 +1,18 @@
 package org.example;
 
-public class ReverseWord {
-}
+
+    import java.util.Scanner;
+
+    public class ReverseWord {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter a word: ");
+            String input = sc.nextLine();
+
+            String reversed = new StringBuilder(input).reverse().toString();
+
+            System.out.println("Reversed word: " + reversed);
+        }
+    }
+
